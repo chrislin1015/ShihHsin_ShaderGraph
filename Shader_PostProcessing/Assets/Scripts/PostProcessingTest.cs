5d8ecf88af0a913d5142ba9ff1638e93006790fe
Chris Lin <chrislin1015@gmail.com>
2019-05-15 11:59:57 +0800

    PP範例場景

---
+++ Shader_PostProcessing/Assets/Scripts/PostProcessingTest.cs
@@ -0,0 +1,46 @@
+﻿#define OVERRIDE_PP
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PostProcessingTest : MonoBehaviour
+{
+    public PostProcessingBehaviour m_PostProcessing;
+#if OVERRIDE_PP
+    protected PostProcessingProfile m_Profile;
+#endif
+
+    // Use this for initialization
+    void Start ()
+    {
+#if OVERRIDE_PP
+        if (m_PostProcessing != null && m_PostProcessing.profile != null)
+        {
+            m_Profile = Instantiate(m_PostProcessing.profile);
+            m_PostProcessing.profile = m_Profile;
+        }
+#endif
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+#if OVERRIDE_PP
+        if (m_Profile != null)
+        {
+            BloomModel.Settings _Bloom = m_Profile.bloom.settings;
+            _Bloom.bloom.intensity = Random.Range(0.5f, 2.0f);
+            m_Profile.bloom.settings = _Bloom;
+        }
+#else
+        if (m_PostProcessing != null && m_PostProcessing.profile != null)
+        {
+            BloomModel.Settings _Bloom = m_PostProcessing.profile.bloom.settings;
+            _Bloom.bloom.intensity = Random.Range(0.5f, 2.0f);
+            m_PostProcessing.profile.bloom.settings = _Bloom;
+        }
+#endif
+    }
+}
---
+++ Shader_PostProcessing/Assets/Scripts/PostProcessingTest2.cs
@@ -0,0 +1,28 @@
+﻿#define OVERRIDE_PP
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingTest2 : MonoBehaviour
+{
+    public PostProcessVolume m_Volume;
+    Bloom m_Bloom;
+
+    // Use this for initialization
+    void Start ()
+    {
+#if OVERRIDE_PP
+        m_Bloom = m_Volume.profile.GetSetting<Bloom>();
+#else
+        m_Bloom = m_Volume.sharedProfile.GetSetting<Bloom>();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        m_Bloom.intensity.value = UnityEngine.Random.Range(2.0f, 5.0f);
+    }
+}
