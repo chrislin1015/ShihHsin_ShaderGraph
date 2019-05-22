bb6004df8148b4b16d1b93c35e45d2bb143e82a1
Chris Lin <chrislin1015@gmail.com>
2019-05-22 19:34:14 +0800

    整理

--- ShaderGraph/Assets/ShaderGraphSample/Script/RadialBlurPP.cs
+++ ShaderGraph/Assets/ShaderGraphSample/Script/RadialBlurPP.cs
@@ -8,8 +8,6 @@ using System;
 [PostProcess(typeof(RadialBlurPPRenderer), PostProcessEvent.BeforeStack, "Custom/RadialBlur")]
 public class RadialBlurPP : PostProcessEffectSettings
 {
-    public Shader m_RadialBlurShader;
-
     public Vector2Parameter m_Center = new Vector2Parameter { value = new Vector2(0.5f, 0.5f) };
 
     [Range(0.0f, 2.0f), Tooltip("Sample Dist")]
@@ -17,24 +15,16 @@ public class RadialBlurPP : PostProcessEffectSettings
 
     [Range(0.0f, 10.0f), Tooltip("Sample Strength")]
     public FloatParameter m_Strength = new FloatParameter { value = 2.2f };
-
-    //public Vector2 m_Center = new Vector2(0.5f, 0.5f);
-    //public float m_Dist = 1.0f;
-    //public float m_Strength = 2.2f;
 }
 
 public sealed class RadialBlurPPRenderer : PostProcessEffectRenderer<RadialBlurPP>
 {
     public override void Render(PostProcessRenderContext iContext)
     {
-        Shader _Shader = Shader.Find("Shader Graphs/RadialBlur");
-        //if (settings.m_RadialBlurShader != null)
-        //{
-            var _Sheet = iContext.propertySheets.Get(Shader.Find("Chris/RadialBlur2"));//.Get(m_RadialBlurShader);//Shader.Find("Hidden/Custom/Grayscale"));
-            _Sheet.properties.SetVector("_Center", new Vector4(settings.m_Center.value.x, settings.m_Center.value.y, 0.0f, 0.0f));
-            _Sheet.properties.SetFloat("_SampleDist", settings.m_Dist);
-            _Sheet.properties.SetFloat("_SampleStrength", settings.m_Strength);
-            iContext.command.BlitFullscreenTriangle(iContext.source, iContext.destination, _Sheet, 0);
-        //}
+        var _Sheet = iContext.propertySheets.Get(Shader.Find("Chris/RadialBlur2"));
+        _Sheet.properties.SetVector("_Center", new Vector4(settings.m_Center.value.x, settings.m_Center.value.y, 0.0f, 0.0f));
+        _Sheet.properties.SetFloat("_SampleDist", settings.m_Dist);
+        _Sheet.properties.SetFloat("_SampleStrength", settings.m_Strength);
+        iContext.command.BlitFullscreenTriangle(iContext.source, iContext.destination, _Sheet, 0);
     }
 }
